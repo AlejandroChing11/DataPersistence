6f59c57f4fb7603f75376f7ad36e49c0f41ecf11
Alejandro Ching <alejandroching2004@hotmail.com>
2023-01-24 14:59:39 -0500

    Adding first querys

--- src/main/java/com/platzi/mensajes_app/MensajeServicio.java
+++ src/main/java/com/platzi/mensajes_app/MensajeServicio.java
@@ -1,8 +1,22 @@
 package com.platzi.mensajes_app;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class MensajeServicio {
     public static void crearMensaje(){
+        Conexion dbConnect = new Conexion();
+        try (Connection conexion = dbConnect.getConnection()) {
+            PreparedStatement ps = null;
+            try () {
+                String query = "INSERT INTO `mensajes` (`id_mensaje`, `mensaje`, `autor_mensaje`, `fecha_mensaje`) VALUES (NULL, 'Hola mundo, primer query', 'Alejandro', current_timestamp());";
+            } catch {
 
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Connection error " + e);
+        }
     }
     public static void listarMensaje(){
 
